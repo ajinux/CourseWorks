5ce7b5487284b5b7d8f6b0ccca85b636973046db
Ajithkumar-sekar <ajithkumarsekark@gmail.com>
2016-07-17 19:48:32 +0530

    Reversing a string

--- operating systems/ReverseString.sh
+++ operating systems/ReverseString.sh
@@ -4,6 +4,7 @@ string=$1
 len=`expr length $string`
 echo $len  
 result=""
+#ref:http://tldp.org/LDP/abs/html/string-manipulation.html
 #reverse a string without using inbuild function
 #echo $string | rev
 for(( i=$len-1; i>=0; i-- ))
