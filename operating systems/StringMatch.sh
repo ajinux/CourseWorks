f393ac22630299589f8f813835f20e5c19abba0a
Ajithkumar-sekar <ajithkumarsekark@gmail.com>
2016-07-17 19:49:49 +0530

    Check two strings are same

--- operating systems/StringMatch.sh
+++ operating systems/StringMatch.sh
@@ -4,7 +4,7 @@ read str1
 echo "Enter the second string:"
 read str2
 
-if [ "$str1" = "$str2" ] #don't leave space [ "$str1" == "$str2" ] 
+if [ "$str1" = "$str2" ] #don't leave space [ "$str1" = "$str2" ] 
 then
   echo "Both the strings are equal"
 else
