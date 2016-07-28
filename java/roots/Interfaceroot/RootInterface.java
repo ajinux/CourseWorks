10dac83224228f504bf52e8b06650e0b84e75dcb
Ajithkumar-sekar <ajithkumarsekark@gmail.com>
2016-07-28 19:07:07 +0530

    Quadratic roots calculation

---
+++ java/roots/Interfaceroot/RootInterface.java
@@ -0,0 +1,7 @@
+package Interfaceroot;
+//import java.lang.String;
+
+public interface RootInterface {
+     int a=0,b=0,c=0;
+     String quadraticroots(double a,double b,double c);
+}
---
+++ java/roots/calcroot.java
@@ -0,0 +1,39 @@
+
+import Interfaceroot.RootInterface;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+class calcroot implements RootInterface 
+{
+   	public String quadraticroots(double a, double b, double c) 
+	{  
+		double d= b*b-4*a*c;
+		if(d>0)
+			return "The roots are real and unequal";
+		else if(d==0)
+			return "The roots are real and equal";
+		else
+			return "There are no real roots";
+	
+	}
+	public static void main(String args[])
+	{
+		Scanner user_input=new Scanner(System.in);
+		double a=0,b=0,c=0;
+		calcroot r1=new calcroot();
+	    System.out.println("Enter a, b and c");
+	    try
+	    {
+	     a=user_input.nextDouble();
+	     b=user_input.nextDouble();
+	     c=user_input.nextDouble();
+		 System.out.println(r1.quadraticroots(a, b, c));
+	    }
+	    catch(InputMismatchException e)
+	    {
+	      System.out.println("Inavlid input --program terminated");
+	    }
+	    
+	}
+   
+}
